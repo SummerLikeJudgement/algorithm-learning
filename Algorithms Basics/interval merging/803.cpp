4c362544ca1fd872df84088a7df53dc43e798aa1
summer <summer35174@gmail.com>
2026-03-10 20:21:14 +0800

    interval merge

---
+++ Algorithms Basics/interval merging/803.cpp
@@ -0,0 +1,84 @@
+// 题目描述
+// 给定 n 个区间 [li,ri]，要求合并所有有交集的区间。
+// 注意如果在端点处相交，也算有交集。
+// 输出合并完成后的区间个数。
+
+// 例如：[1,3]和[2,6]可以合并为一个区间[1,6]。
+
+// 输入格式
+// 第一行包含整数n。
+
+// 接下来n行，每行包含两个整数 l 和 r。
+
+// 输出格式
+// 共一行，包含一个整数，表示合并区间完成后的区间个数。
+
+// 输入输出样例
+// 输入
+// 5
+// 1 2
+// 2 4
+// 5 6
+// 7 8
+// 7 9
+// 输出
+// 3
+// 说明/提示
+// 1≤n≤100000,
+// −10^9≤li≤ri≤10^9
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> PII;
+
+const int N = 100010;
+vector<PII> a;
+
+void merge(vector<PII> &segs)
+{
+    vector<PII> res;
+
+    sort(segs.begin(), segs.end());
+
+    int st = -2e9, ed = -2e9;
+    for (auto seg : segs)
+    {
+        if (ed < seg.first)
+        {
+            if (st != -2e9)
+                res.push_back({st, ed}); // 第一个区间初始化
+            st = seg.first;
+            ed = seg.second;
+        }
+        else ed = max(ed, seg.second);
+    }
+
+    if (st != -2e9) res.push_back({st, ed});
+
+    segs = res;
+
+    return;
+}
+
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for (int i=0 ; i<n ; i++)
+    {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        a.push_back({l, r});
+    }
+
+    merge(a);
+
+    printf("%d", a.size());
+
+    return 0;
+}
